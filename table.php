e7350fa41b1eeb2d3e63b1621553777ca0d1dbb3
sanilakhanal <janaurygirl123@gmail.com>
2017-01-24 13:06:36 +0545

    second commit

---
+++ table.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--html table-->
+<html>
+<head>
+	<title>table</title>
+
+
+
+
+</head>
+<body>
+	<div class="rightside-bar">
+
+		<h1>Personal Information</h1>
+		<table border="1" style="width: 60%" " bordercolor="blue">
+					<thead>
+
+					<tr>
+						<th> first name</th>
+						<th>last name</th>
+						<th> address</th>
+						<th> email</th>
+						<th> phone no.</th>
+						
+					</tr>
+					</thead>
+
+
+					<tr>
+						<td>Sanila</td>
+						<td>Khanal</td>
+						<td>Koteshowr</td>
+						<td> sanila425@gmail.com</td>
+						<td>9849074444</td>
+					</tr>
+					<tr>
+						<td>Rebs</td>
+						<td>Dulal</td>
+						<td>Patan</td>
+						<td> pie22@gmail.com</td>
+						<td>9849074465</td>
+					</tr>
+					<tr>
+						<td>Rju</td>
+						<td>Thapa</td>
+						<td>Kapan</td>
+						<td> rj325@gmail.com</td>
+						<td>9849047574</td>
+			
+					</tr>
+
+		</table>
+
+	</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
